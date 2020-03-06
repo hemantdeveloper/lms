62187c87ab470f22555a2e8967df0608133152d5
Hemant Pratap Singh <61836063+hemantdeveloper@users.noreply.github.com>
2020-03-06 19:16:25 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   <a> Designed by : HEMANT PRATAP SINGH</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
